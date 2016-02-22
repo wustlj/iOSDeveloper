23bc4f78fedaa4cfa56d16c178521672c59a9d8e
wustlj <wustlj@163.com>
2016-02-22 18:57:53 +0800

    fix

--- YKWebImage/YKWebImage/YKImageDownloadOperation.m
+++ YKWebImage/YKWebImage/YKImageDownloadOperation.m
@@ -8,14 +8,23 @@
 
 #import "YKImageDownloadOperation.h"
 
-@interface YKImageDownloadOperation ()
+@interface YKImageDownloadOperation () <NSURLConnectionDataDelegate>
+
+@property (nonatomic, assign, getter=isExecuting) BOOL executing;
+@property (nonatomic, assign, getter=isFinished) BOOL finished;
 
 @property (nonatomic, copy) YKImageDownloadProgressBlock progressBlock;
 @property (nonatomic, copy) YKImageDownloadCompletedBlock completedBlock;
 
+@property (nonatomic, strong) NSURLConnection *urlConnection;
+@property (nonatomic, strong) NSMutableData *receivedData;
+
 @end
 
 @implementation YKImageDownloadOperation
+
+@synthesize finished = _finished;
+@synthesize executing = _executing;
 
 - (id)initWithRequest:(NSURLRequest *)request progress:(YKImageDownloadProgressBlock)progressBlock completed:(YKImageDownloadCompletedBlock)completedBlock {
     self = [super init];
@@ -23,8 +32,55 @@
         _request = request;
         _progressBlock = [progressBlock copy];
         _completedBlock = [completedBlock copy];
+        
     }
     return self;
+}
+
+- (void)start {
+    @synchronized(self) {
+        if (self.isCancelled) {
+            self.finished = YES;
+            return;
+        }
+        
+        self.urlConnection = [[NSURLConnection alloc] initWithRequest:self.request delegate:self startImmediately:NO];
+    }
+    
+    [self.urlConnection start];
+}
+
+- (void)setFinished:(BOOL)finished {
+    [self willChangeValueForKey:@"isFinished"];
+    _finished = finished;
+    [self didChangeValueForKey:@"isFinished"];
+}
+
+- (void)setExecuting:(BOOL)executing {
+    [self willChangeValueForKey:@"isExecuting"];
+    _executing = executing;
+    [self didChangeValueForKey:@"isExecuting"];
+}
+
+#pragma mark - NSURLConnectionDataDelegate
+
+- (void)connection:(NSURLConnection *)connection didReceiveResponse:(NSURLResponse *)response {
+    self.receivedData = [[NSMutableData alloc] init];
+}
+
+- (void)connection:(NSURLConnection *)connection didReceiveData:(NSData *)data {
+    [self.receivedData appendData:data];
+}
+
+- (void)connectionDidFinishLoading:(NSURLConnection *)connection {
+    if (_completedBlock) {
+        UIImage *image = [UIImage imageWithData:self.receivedData];
+        _completedBlock(image, YES);
+    }
+}
+
+- (void)connection:(NSURLConnection *)connection didFailWithError:(NSError *)error {
+    self.receivedData = nil;
 }
 
 @end
